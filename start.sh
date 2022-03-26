5bcc0bc54d640efef4a7d8aa56c1826b27645e7b
Mike Wilcox <mwilcox56@gmail.com>
2022-03-26 11:06:10 -0400

    testing

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# https://stackoverflow.com/a/55794238
+npm config set scripts-prepend-node-path auto
 yarn install
+# NEEDS .env here
 yarn nft-market:client-prod
+# SUDO
 mkdir -p /var/www/foamies
 cp -r packages/nft-market/client/dist/. /var/www/foamies
 yarn nft-market:server-prod
