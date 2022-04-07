9e2049b720e9bbee8cb89e833f06767930e5633e
Mike Wilcox <mwilcox56@gmail.com>
2022-04-06 20:41:25 -0400

    add multi-copy

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Use this script to start docker and spin up
 
+echo ./packages/nft-market/client/ ./packages/nft-market/image-api/ ./packages/nft-market/server/ | xargs -n 1 cp .env
 docker-compose build
 docker-compose up -d
