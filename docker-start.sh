c80fa34112e3f909130b319d739aaebf01017bda
Mike Wilcox <mwilcox56@gmail.com>
2022-04-09 17:18:38 -0400

    cleanup

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-# Use this script to start docker and spin up
 
-echo ./packages/nft-market/client/ ./packages/nft-market/img-api-server/ ./packages/nft-market/fe-server/ | xargs -n 1 cp .env
+cp .grass/.env.client ./packages/nft-market/client/.env
+cp .grass/.env.img-api-server ./packages/nft-market/img-api-server/.env
+cp .grass/.env.fe-server ./packages/nft-market/fe-server/.env
+touch .env
+cat .grass/.env.fe-server-mysql >> .env
+cat .grass/.env.fe-server-mysql >> ./packages/nft-market/fe-server/.env
+cat .grass/.env.img-api-server-mysql >> .env
+cat .grass/.env.img-api-server-mysql >> ./packages/nft-market/img-api-server/.env
 docker-compose build
 docker-compose up -d
+rm ./packages/nft-market/client/.env
+rm ./packages/nft-market/img-api-server/.env
+rm ./packages/nft-market/fe-server/.env
+rm .env
