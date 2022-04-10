c9bb1f7fb21d37eba2341f96db0b67f3071c7285
Mike Wilcox <mwilcox56@gmail.com>
2022-04-10 16:02:59 -0400

    remember urlencoded

--- packages/nft-market/img-api-server/src/routes/graphql.ts
+++ packages/nft-market/img-api-server/src/routes/graphql.ts
@@ -1,10 +1,11 @@
 import { graphqlHTTP } from 'express-graphql';
-import { json, Router } from 'express';
+import { json, urlencoded, Router } from 'express';
 import { isProduction } from '../util';
 import schema from '../schema/index';
 
 const router = Router();
 router.use(json({ limit: '100mb' }));
+router.use(urlencoded({ limit: '100mb', extended: false }));
 
 router.get('/', graphqlHTTP({
     schema,
