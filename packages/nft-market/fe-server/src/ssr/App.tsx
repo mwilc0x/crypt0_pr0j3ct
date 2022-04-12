7054b83586b413d4d58466ba317537bcc36fed1c
Mike Wilcox <mwilcox56@gmail.com>
2022-04-12 07:30:47 -0400

    organizing ssr code on fe-server

--- packages/nft-market/fe-server/src/ssr/App.tsx
+++ packages/nft-market/fe-server/src/ssr/App.tsx
@@ -1,18 +1,10 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import React from 'react';
 import Html from './Html';
 
  export default function App({assets}) {
    const components = [];
  
-   for (let i = 0; i <= 1000; i++) {
+   for (let i = 0; i <= 100; i++) {
      components.push(<HeavyComponent />);
    }
  
