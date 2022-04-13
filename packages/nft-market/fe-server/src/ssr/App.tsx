8181f6da0c3c91801c1f2fdc8017162ab85a4b60
Mike Wilcox <mwilcox56@gmail.com>
2022-04-13 19:30:47 -0400

    test

--- packages/nft-market/fe-server/src/ssr/App.tsx
+++ packages/nft-market/fe-server/src/ssr/App.tsx
@@ -1,54 +1,12 @@
 import React from 'react';
+import Home from '@crypto-org/nft-market-client/src/pages/Home';
 import Html from './Html';
 
- export default function App({assets}) {
-   const components = [];
- 
-   for (let i = 0; i <= 100; i++) {
-     components.push(<HeavyComponent />);
-   }
- 
-   return (
-     <Html assets={assets} title="Hello">
-       <h1>React 18 stream</h1>
-       {components}
-     </Html>
-   );
- }
- 
- function HeavyComponent() {
-   return (
-     <>
-       <ul>
-         <li>
-           <a href="#">Link</a>
-         </li>
-         <li>
-           <a href="#">Link</a>
-         </li>
-         <li>
-           <a href="#">Link</a>
-         </li>
-         <li>
-           <a href="#">Link</a>
-         </li>
-         <li>
-           <a href="#">Link</a>
-         </li>
-       </ul>
-       <article>
-         <h1>Title</h1>
-         <p>Content</p>
-         <div>
-           <div>
-             <div>
-               <div>
-                 <div>12345</div>
-               </div>
-             </div>
-           </div>
-         </div>
-       </article>
-     </>
-   );
- }
+export default function App({assets}) { 
+  return (
+    <Html assets={assets} title="Hello">
+      <h1>React 18 stream</h1>
+      <Home />
+    </Html>
+  );
+}
