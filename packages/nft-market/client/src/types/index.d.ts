f58a5ccd95ea0617d06958d52ec031049b40ea47
Mike Wilcox <mwilcox56@gmail.com>
2022-04-14 08:20:47 -0400

    change typings back

---
+++ packages/nft-market/client/src/types/index.d.ts
@@ -0,0 +1,47 @@
+type FileUpload = {
+    name: string;
+    data: any;
+};
+
+type NFTForSave = {
+    name: string;
+    description: string;
+    id: string;
+    price: number;
+}
+
+interface FullContract {
+    [key: string]: {
+        [key: string]: { contracts: {
+            [key: string]: { address: string, abi: Object[] }
+        }}
+    }
+}
+
+interface User {
+    username: string;
+    id: string;
+}
+
+interface PartialContract {
+    address: string,
+    abi: Object[]
+}
+
+type Listing = {
+    name: string;
+    description: string;
+    tokenId: number;
+    tokenUri: string;
+    seller: string;
+    owner: string;
+    price: number|string;
+    forSale: boolean;
+}
+
+type FormSubmission = {
+    name: string,
+    description: string,
+    file: FileUpload,
+    price: string
+};
