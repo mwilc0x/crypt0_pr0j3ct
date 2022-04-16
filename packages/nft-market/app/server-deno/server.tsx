0e9465c75cd956e4081c6b8e8a0e55e750077f32
Mike Wilcox <mwilcox56@gmail.com>
2022-04-16 08:59:26 -0400

    try more advanced config

--- packages/nft-market/app/server-deno/server.tsx
+++ packages/nft-market/app/server-deno/server.tsx
@@ -21,9 +21,34 @@ app.use(async (ctx, next) => {
 });
 
 app.use(async (ctx: any, next) => {
-  const wat = await renderToReadableStream(<h1>hello!</h1>);
-  console.log('wat:', wat);
-  ctx.send(wat);
+  // The new wiring is a bit more involved.
+  ctx.socket.on('error', (error: any) => {
+    console.error('Fatal', error);
+  });
+
+  let didError = false;
+
+  const { pipe, abort } = await renderToReadableStream(
+    <h1>hello!</h1>,
+    {
+      bootstrapScripts: [],
+      onShellReady() {
+        // If something errored before we started streaming, we set the error code appropriately.
+        ctx.response.statusCode = didError ? 500 : 200;
+        ctx.response.setHeader('Content-type', 'text/html');
+        pipe(ctx.response);
+      },
+      onShellError(_: any) {
+        // Something errored before we could complete the shell so we emit an alternative shell.
+        ctx.response.statusCode = 500;
+        ctx.response.send('<!doctype><p>Error</p>');
+      },
+      onError(error: any) {
+        didError = true;
+        console.error(error);
+      },
+    }
+  );
 });
 
 const port: number = Number(config()['APP_SSR_SERVER_PORT_PROD']);
