a873cf2a9722deab85ec1ca34ef557c080864f94
Mike Wilcox <mwilcox56@gmail.com>
2022-04-16 07:54:28 -0400

    add some timing and logging

--- packages/nft-market/app/server-deno/server.ts
+++ packages/nft-market/app/server-deno/server.ts
@@ -3,6 +3,21 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 app.use((ctx) => {
   ctx.response.body = '<html><body><h1>Yo!</h1></body></html>';
 });
