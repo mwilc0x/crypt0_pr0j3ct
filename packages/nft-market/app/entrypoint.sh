15281747b52503ed1cfd6c56d64e24298cdeae4a
Mike Wilcox <mwilcox56@gmail.com>
2022-04-16 13:58:20 -0400

    more cleanup

--- packages/nft-market/app/entrypoint.sh
+++ packages/nft-market/app/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 APP_SSR_SERVER_PORT_PROD=5075
-deno run --config "./server-deno/deno.json" --allow-read --allow-env --allow-net server-deno/server.tsx
+deno run --config "./server/deno.json" --allow-read --allow-env --allow-net server/server.tsx
