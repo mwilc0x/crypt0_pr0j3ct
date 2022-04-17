a77ff884a449e568e01efa2d84d2f69eb42db926
Mike Wilcox <mwilcox56@gmail.com>
2022-04-17 12:10:19 -0400

    testing

--- packages/nft-market/app/entrypoint.sh
+++ packages/nft-market/app/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 APP_SSR_SERVER_PORT_PROD=5075
-deno run --config "./server/deno.json" --allow-read --allow-env --allow-net server/server.tsx
+cd ./ultra-examples
+deno task dev
+# deno run --config "./server/deno.json" --allow-read --allow-env --allow-net server/server.tsx
