efe552c125c36ea2276f36228292d9a5ca4d2fb4
Mike Wilcox <mwilcox56@gmail.com>
2022-04-17 19:27:14 -0400

    renaming

--- packages/nft-market/app/entrypoint.sh
+++ packages/nft-market/app/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 APP_SSR_SERVER_PORT_PROD=5075
-cd ./ultra-examples
+cd ./full-stack
 deno task dev
-# deno run --config "./server/deno.json" --allow-read --allow-env --allow-net server/server.tsx
