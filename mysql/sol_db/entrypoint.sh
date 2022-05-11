6cc60211c2dde2c732d260d881213e154b34bb41
Mike Wilcox <mwilcox56@gmail.com>
2022-05-11 11:11:59 -0400

    install curl

--- mysql/sol_db/entrypoint.sh
+++ mysql/sol_db/entrypoint.sh
@@ -4,8 +4,9 @@ apt-get update
 apt-get -y install keychain
 apt-get -y install git
 apt-get -y install nodejs
+apt-get -y install curl
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
-source ~/.nvm/nvm.sh
+source ~/.bashrc
 nvm install --lts
 /usr/bin/mysqld_safe --skip-grant-tables &
 echo "Solana database created"
