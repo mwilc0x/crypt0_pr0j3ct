9761bcde906e60b5389a37f5f0df2f62cdd88114
Mike Wilcox <mwilcox56@gmail.com>
2022-05-13 08:10:49 -0400

    more trying

--- actions/soldb/go.js
+++ actions/soldb/go.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
 require('dotenv/config');
-console.log('howdy from nodejs land');
-console.log(process.env);
+
+async function main() {
+    console.log('howdy from nodejs land');
+    console.log(process.env);
+
+    const eventData = await readfile(process.env.GITHUB_EVENT_PATH);
+    console.log(eventData);
+}
+
+function readFile(path) {
+    return new Promise((resolve) => {
+        try {
+            fs.readFile(path, {}, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    resolve([]);
+                    return;
+                }
+                const json = JSON.parse(data.toString());
+                resolve(json);
+            });
+        } catch (error) {
+            console.log(error);
+            resolve([]);
+        }
+    });
+}
