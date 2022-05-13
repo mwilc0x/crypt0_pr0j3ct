9d9a9abc4a546807b580abb164e98fc82d2636da
Mike Wilcox <mwilcox56@gmail.com>
2022-05-13 16:53:41 -0400

    re-wire

--- actions/soldb/go.js
+++ actions/soldb/go.js
@@ -1,43 +1,20 @@
 const fs = require('fs');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv/config');
 
-let internalPool;
 main();
 async function main() {
     console.log('howdy from nodejs land');
     const eventData = await readFile(process.env.GITHUB_EVENT_PATH);
 
-    internalPool = mysql.createPool({
+    const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
         user: process.env.MYSQL_USER,
-        database: process.env.MYSQL_DATABASE,
-        password: process.env.MYSQL_PASSWORD,
-        waitForConnections: true
+        database: process.env.MYSQL_DATABASE
     });
-
-    console.log('mysql', internalPool);
-
-    // Allows better control of openned connections
-    const threadId = await registerThreadCounter();
-    console.log(`received back threadId ${threadId}`);
-}
-
-async function registerThreadCounter() {
-    return new Promise((resolve) => {
-        try {
-            internalPool.on('connection', (connection) => {
-                console.log(`New connection stablished with server on thread #${connection.threadId}`);
-                resolve(connection.threadId);
-                return;
-            });
-        } catch (error) {
-            console.log(`error connecting to mysql ${error}`);
-            resolve(error);
-            return;
-        }
-    });
+    // query database
+    const [rows, fields] = await connection.execute('SELECT * FROM `collections`');
+    console.log(rows, fields);;
 }
 
 function readFile(path) {
@@ -57,133 +34,4 @@ function readFile(path) {
             resolve([]);
         }
     });
-}
-
-class MySQLWrapper {
-    
-    /**
-     * 
-     * Queries the database
-     * @param {String} query - The query itself
-     * @param {Array} params - The parameters to be passed to MySQL
-     * @returns {Promise} - A promise to a query result
-     * 
-     */
-    static createQuery({ query, params }) {
-        return new Promise((succeed, fail) => {
-            mySQLConnector.pool.getConnection((err, connection) => {
-                if (err) {
-                    return fail(err);
-                }
-                connection.query(query, params, (err, rows) => {
-                    connection.release();
-                    if (err) {
-                        return fail(err);
-                    }
-                    return succeed(rows);
-                });
-            });
-        });
-    }
-
-    /**
-     * 
-     * Runs a transactional query
-     * @param {MySQL.Connection} connection - The connection whose transaction will be used
-     * @param {String} query - The query itself
-     * @param {Array} params - The parameters to be passed to MySQL
-     * @returns {Promise} - A promise to a query result
-     * 
-     */
-    static createTransactionalQuery({ query, params, connection }) {
-        return new Promise((succeed, fail) => {
-            connection.query(query, params, (err, rows) => {
-                if (err) {
-                    return fail(err);
-                }
-                return succeed(rows);
-            });
-        });
-    }
-    
-    /**
-     * 
-     * Rollbacks a transaction
-     * @param {MySQL.Connection} connection - The connection whose transaction will be rollbacked
-     * @returns {Promise} - A promise to the rollback
-     * 
-     */
-    static rollback(connection) {
-        return new Promise((succeed, fail) => {
-            try {
-                connection.rollback(() => {
-                    return succeed();
-                });
-            } catch (e) {
-                return fail(e);
-            } finally {
-                connection.release();
-            }
-        });
-    }
-
-    /**
-     * 
-     * Commits a transaction
-     * @param {MySQL.Connection} connection - The connection whose transaction will be commited
-     * @returns {Promise} - A promise to the commit
-     * 
-     */
-    static commit(connection) {
-        return new Promise((succeed, fail) => {
-            try {
-                connection.commit(err => { 
-                    if (err) { 
-                        return this.rollback(connection);
-                    }
-                    return succeed();
-                })
-            } catch (e) {
-                return fail(e);
-            } finally {
-                connection.release();
-            }
-
-        })
-
-    }
-
-    /**
-     * 
-     * Retrieves a connection from the pool to be used in transactions
-     * @param {MySQL.Connection} connection - A connection from the pool
-     * 
-     */
-    static getConnectionFromPool() {
-        return new Promise((succeed, fail) => {
-            mySQLConnector.pool.getConnection((err, connection) => {
-                if (err) {
-                    return fail(err);
-                }
-                return succeed(connection);
-            });
-        });
-    }
-
-    /**
-     * 
-     * Begins a new transaction in a connection
-     * @param {MySQL.Connection} connection - A connection from the pool
-     * 
-     */
-    static beginTransaction(connection) {
-        return new Promise((succeed, fail) => {
-            connection.beginTransaction(err => {
-                if (err) {
-                    return fail(err);
-                }
-                return succeed(connection);
-            });
-        });
-    }
 }
