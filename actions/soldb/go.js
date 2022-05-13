6940efcb8a659a259d6e30abb496f96369d091c4
Mike Wilcox <mwilcox56@gmail.com>
2022-05-13 16:57:10 -0400

    use password

--- actions/soldb/go.js
+++ actions/soldb/go.js
@@ -10,6 +10,7 @@ async function main() {
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
     });
     // query database
