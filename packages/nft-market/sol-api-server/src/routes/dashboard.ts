18eb2735a3d35ea5fdd37287f722ae0af7ddf347
Mike Wilcox <mwilcox56@gmail.com>
2022-05-21 15:07:28 -0400

    fix broken import

--- packages/nft-market/sol-api-server/src/routes/dashboard.ts
+++ packages/nft-market/sol-api-server/src/routes/dashboard.ts
@@ -1,12 +1,10 @@
 import { json, Request, Response, Router } from 'express';
-import DashboardUI from '../dashboard-ui';
 
 const router = Router();
 router.use(json({ limit: '100mb' }));
 
 router.get('/login', (req: Request, res: Response) => {
     try {
-      DashboardUI();
       res.status(200).json({ success: true, data: {} });
     } catch (error: any) {
       res.status(500);
