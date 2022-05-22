cfecb2fa9856851e75ca34702381949f7fad15d1
Mike Wilcox <mwilcox56@gmail.com>
2022-05-22 13:59:36 -0400

    fix sql file

--- postgres/sol_api_userdb/init.sql
+++ postgres/sol_api_userdb/init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `users` (
-  `id` CHAR(42) NOT NULL PRIMARY KEY,
-  `password` CHAR(50) NOT NULL
+CREATE TABLE users (
+  id CHAR(42) NOT NULL PRIMARY KEY,
+  password CHAR(50) NOT NULL
 );
 
-INSERT INTO `users` (`id`, `password`) VALUES 
+INSERT INTO users (id, password) VALUES 
 ('mwilcox56@gmail.com', 'testing123');
