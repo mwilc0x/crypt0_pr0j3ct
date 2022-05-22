eaacca764a1293ea1255773262959f4ccd682f5b
Mike Wilcox <mwilcox56@gmail.com>
2022-05-22 11:44:07 -0400

    test add postgres

---
+++ postgres/sol_api_userdb/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` CHAR(42) NOT NULL PRIMARY KEY,
+  `password` CHAR(50) NOT NULL
+);
+
+INSERT INTO `users` (`id`, `password`) VALUES 
+('mwilcox56@gmail.com', 'testing123');
