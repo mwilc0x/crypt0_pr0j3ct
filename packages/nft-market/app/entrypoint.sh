2610b196164bf773434159719ccbdbf91d19a637
Mike Wilcox <mwilcox56@gmail.com>
2022-06-23 16:51:11 -0400

    updates

--- packages/nft-market/app/entrypoint.sh
+++ packages/nft-market/app/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-APP_SSR_SERVER_PORT_PROD=5075
-cd ./full-stack
-deno task start
+# APP_SSR_SERVER_PORT_PROD=5075
+# cd ./full-stack
+cd ./next-market
+yarn install
+yarn run build
+yarn run start
+# deno task start
